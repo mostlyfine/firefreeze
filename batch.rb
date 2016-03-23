03fd4c08e0befa5db55da1cc1657f6b24f559017
Seiji Sawayanagi <mostlyfine@gmail.com>
2016-03-23 20:38:10 +0900

    Improved to use the user-agent with access to the Hatena

--- batch.rb
+++ batch.rb
@@ -1,10 +1,12 @@
 require 'rss'
 require 'net/http'
 require 'uri'
+require 'open-uri'
 require_relative 'models/init'
 
 post_uri = URI.parse('https://archive.is/submit/')
-rss = RSS::Parser.parse('http://b.hatena.ne.jp/search/tag?safe=on&q=%E7%82%8E%E4%B8%8A&mode=rss&users=3')
+body = open("http://b.hatena.ne.jp/search/tag?safe=off&q=%E7%82%8E%E4%B8%8A|%E3%83%88%E3%83%A9%E3%83%96%E3%83%AB&mode=rss&users=3", 'User-Agent' => 'Mozilla/4.0 (compatible; MSIE 8.0; Windows NT 6.1; Trident/4.0)').read
+rss = RSS::Parser.parse(body)
 rss.items.each do |item|
   begin
     link = URI.escape(item.link)
